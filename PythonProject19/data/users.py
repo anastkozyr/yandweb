a61ac21cd4694652af3a05bf3a7b055a801365d4
anastkozyr <kozyr.2009@bk.ru>
2026-03-19 00:46:08 +0300

    register

--- PythonProject19/data/users.py
+++ PythonProject19/data/users.py
@@ -1,6 +1,8 @@
 import datetime
 from sqlalchemy import orm
 import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
 
 
@@ -21,3 +23,9 @@ class User(SqlAlchemyBase):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
     jobs = orm.relationship("Jobs", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- PythonProject19/main.py
+++ PythonProject19/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
+from PythonProject19.forms.user import RegisterForm
 from data.jobs import Jobs
 from data.users import User
 
@@ -21,7 +22,36 @@ def main():
         names = {name.id: (name.surname, name.name) for name in users}
         return render_template("index.html", jobs=jobs, names=names)
 
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegisterForm()
+        if form.validate_on_submit():
+            if form.password.data != form.password_again.data:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Пароли не совпадают")
+            db_sess = db_session.create_session()
+            if db_sess.query(User).filter(User.email == form.email.data).first():
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Такой пользователь уже есть")
+            user = User(
+                email=form.email.data,
+                surname=form.surname.data,
+                name=form.name.data,
+                age=form.age.data,
+                position=form.position.data,
+                speciality=form.speciality.data,
+                address=form.address.data
+            )
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            return redirect('/login')
+        return render_template('register.html', title='Регистрация', form=form)
+
     app.run()
+
 
 if __name__ == '__main__':
     main()
