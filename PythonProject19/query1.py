6da9303a6285b52f453102b95920d425e7127945
anastkozyr <kozyr.2009@bk.ru>
2026-03-21 18:24:22 +0300

    123

--- PythonProject19/query1.py
+++ PythonProject19/query1.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy import or_
 
 from data.jobs import Jobs
 from data.users import User
@@ -10,26 +11,25 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    db_session.global_init(input())
-    db_sess = db_session.create_session()
-    for user in db_sess.query(User).filter(User.address == "module_1"):
-        print(user.name)
+    global_init(input())
+    db_sess = create_session()
+    for user in db_sess.query(User).filter((User.position.contains('chief')) | (User.position.contains('middle'))):
+        print(user, user.position)
+
 
 if __name__ == '__main__':
     main()
 
+# def main():
+#     global_init(input())
+#
+#     db_sess = create_session()
+#
+#     for user in db_sess.query(User).filter(User.address == "module_1",
+#                                            User.speciality.notlike("%engineer%"),
+#                                            User.position.notlike("%engineer%")):
+#         print(user.id)
 
-def main():
-    global_init(input())
-
-    db_sess = create_session()
-
-    for user in db_sess.query(User).filter(User.address == "module_1",
-                                           User.speciality.notlike("%engineer%"),
-                                           User.position.notlike("%engineer%")):
-        print(user.id)
-
-
-    if __name__ == '__main__':
-        main()
-
+#
+# if __name__ == '__main__':
+#     main()
